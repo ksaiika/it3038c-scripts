bed4f2d9dbeb626b82a2d62c39c37ffb7622eae1
ksaiika <kim2ks@mail.uc.edu>
2023-09-23 22:21:01 -0400

    Lab 3

---
+++ powershell/L3_Script.ps1
@@ -0,0 +1,37 @@
+﻿function getIP {
+    (Get-NetIPAddress).ipv4address | Select-String "192*"
+    }
+
+function user {
+    (whoami).Split('\')[1]
+    }
+
+
+function host {
+    hostname
+    }
+
+
+function powerShellVersion {
+    $HOST.Version.Major
+    }
+
+
+function date {
+    get-date -format "dddd MM/dd/yyyy HH:mm"
+    }
+
+
+$ip = getIP
+$uname = user
+$hname = host
+$PSV = powerShellVersion
+$DT = date
+
+function body {
+    "This machine's IP is $ip. User is $uname. Hostname is $hname. Powershell Version $PSV. Today's Date is $DT" | out-file C:\it3038c-scripts\powershell\Lab3.txt
+    }
+
+$body = body
+
+$body 
