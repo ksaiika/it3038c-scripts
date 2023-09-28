69d07902561fb571093cefd19c68fccc79d0d222
ksaiika <kim2ks@mail.uc.edu>
2023-09-28 10:39:58 -0400

    L5 Python scripts

--- python/nameage.py
+++ python/nameage.py
@@ -6,15 +6,36 @@ print('What is your name?')
 
 myName=input()
 
+while myName != 'Kseniya':
+  if myName == 'Ksen':
+    print('Ha ha, very funny. Seriously, who are you?')
+    myName=input()
+  else:
+    print('That is not your name. Please, tell me your real name.')
+    myName=input()
+
 print('Hello, ' + myName + '. That is a good name. How old are you?')
+
+#print('Hello, ' + myName + '. That is a good name. How old are you?')
 
 myAge=int(input())
 
 programAge = int(time.time() - start_time)
 
-print('%s? That\'s funny, I\'m only %s seconds old.' % (myAge, programAge))
+if myAge < 13:
+    print('Learning young, that\'s good')
+elif myAge == 13:
+    print('You\'re a teenager now... that\'s cool, I guess')
+elif myAge > 13 and myAge <30:
+    print('Still young, still learning...')
+elif myAge >= 30 and myAge < 65:
+    print('Now you\'re adulting.')
+else:
+    print('   you\'ve lived a long time?')
 
-print('I wish I was %s years old' % (myAge *2))
+#print('%s? That\'s funny, I\'m only %s seconds old.' % (myAge, programAge))
+
+#print('I wish I was %s years old' % (myAge *2))
 
 time.sleep(3)
 print('I\'m tired. I go sleep now.')
---
+++ python/yourname.py
@@ -0,0 +1,7 @@
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'Kseniya':
+     break
+
+print('Thank You')
