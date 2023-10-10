299d8205b197c620cb0fbd40e15e22cbaf753484
ksaiika <kim2ks@mail.uc.edu>
2023-10-09 21:13:33 -0400

    documentation

---
+++ project1/howToRunThisScript.md
@@ -0,0 +1,7 @@
+# Follow these steps inside your terminal to run the script in time.py
+======================================================================
+
+1. Open terminal by right-clicking on your linux desktop
+2. Type 'Python3 time.py' to begin the script
+3. Answer the questions
+4. Ta-Da!
--- project1/time.py
+++ project1/time.py
@@ -3,7 +3,9 @@ import time
 
 print("Hello. Would you like to know the time? (y/n)")
 
-answer = input
+answer = input()
+
+"""Used this website to find the time modules and conversion: https://www.freecodecamp.org/news/how-to-get-the-current-time-in-python-with-datetime/#:~:text=To%20get%20the%20current%20time%20in%20particular%2C%20you%20can%20use,hours%2C%20minutes%2C%20and%20seconds"""
 
 currently = datetime.now()
 currentH = currently.strftime("%H")
