8f53d3205188f9a83aa6ba5aa7a148058c67d8b7
ksaiika <kim2ks@mail.uc.edu>
2023-10-11 19:59:24 -0400

    Lab6

--- node/server.js
+++ node/server.js
@@ -5,14 +5,22 @@ var ip = require('ip');
 
 http.createServer(function(req, res){
 
-	if(req.url === "/") {
-		fs.readFile("./public/index.html", "UTF-8", function(err, body){
+	if (req.url === "/") {
+		fs.readFile("./Public/index.html", "UTF-8", function(err, body){
 		res.writeHead(200, {"Content-Type": "text/html"});
 		res.end(body);
 	});
 }
 	else if(req.url.match("/sysinfo")) {
 		myHostName=os.hostname();
+		sUptime = os.uptime();
+		sUptimeD = Math.floor(sUptime / (3600 * 24));
+		sUptimeH = Math.floor((sUptime % (3600 *24)));
+		sUptimeM = Math.floor((sUptime % 3600) / 60);
+		sUptimeS = Math.floor((sUptime % 60));
+		totalMem = (os.totalmem() / 1024 / 1024).toFixed(2);
+		freeMem = (os.freemem() / 1024 / 1024).toFixed(2);
+		cpus = os.cpus().length
 		html=`
 		<!DOCTYPE html>
 		<html>
@@ -22,10 +30,10 @@ http.createServer(function(req, res){
 			<body>
 				<p>Hostname: ${myHostName}</p>
 				<p>IP: ${ip.address()}</p>
-				<p>Server Uptime: </p>
-				<p>Total Memory: </p>
-				<p>Free Memory: </p>
-				<p>Number of CPUs: </p>
+				<p>Server Uptime: Days: ${sUptimeD}, Hours: ${sUptimeH}, Minutes: ${sUptimeM}, Seconds: ${sUptimeS}</p>
+				<p>Total Memory: ${totalMem} MB</p>
+				<p>Free Memory: ${freeMem} MB</p>
+				<p>Number of CPUs: ${cpus}</p>
 			</body>
 		</html>`
 		res.writeHead(200, {"Content-Type": "text/html"});
@@ -37,4 +45,4 @@ http.createServer(function(req, res){
 	}
 }).listen(3000);
 
-console.log("Server listening on port 3000")
+console.log("Server listening on port 3000");
